e17236dc1c18bf551dad5655824b40c2bb46a9f5
princekx <prince.xavier@gmail.com>
2021-09-23 08:18:03 +0100

    Included percentile ratios

--- main.py
+++ main.py
@@ -7,22 +7,21 @@ import mjo.do_mjo as do_mjo
 if __name__ == '__main__':
     control = {}
     control['runid'] = 'u-bw062'
+    control['label'] = 'None'
     control['start_date'] = '1981/12/01'
     control['end_date'] = '2008/12/01'
     control['data_retrieve_dir'] = '/scratch/hadpx/hadgem3/data/SEAPy'
 
-    #control['start_date'] = '2022/01/01'
-    #control['end_date'] = '2041/12/01'
-    #control['data_retrieve_dir'] = '/scratch/hadpx/hadgem3/data/SEAPy'
-
     expt = {}
     expt['runid'] = 'u-by782'
+    expt['label'] = 'None'
     expt['start_date'] = '1981/12/01'
     expt['end_date'] = '2008/12/01'
     expt['data_retrieve_dir'] = '/scratch/hadpx/hadgem3/data/SEAPy'
 
     obs = {}
     obs['runid'] = 'obs'
+    obs['label'] = 'ERAInt/TRMM'
     obs['start_date'] = '1989/01/01'
     obs['end_date'] = '1998/12/01'
     obs['data_retrieve_dir'] = '/project/MJO_GCSS/hadgem3/data/obs/SEAPy_data'
@@ -35,14 +34,14 @@ if __name__ == '__main__':
 
     # 2. Do MJO calculations
     # put obs=None if you do not wish to compute obs every time
-    #do_mjo.mjo_compute(control=None, expt=None, obs=obs,
-    #                   level1=False, level2=False, level3=True)
+    do_mjo.mjo_compute(control=control, expt=expt, obs=obs,
+                       level1=True, level2=True, level3=True)
 
     # 3. Do SEA computations
     # put obs=None if you do not wish to compute obs every time
     #do_sea.sea_compute(varnames, control=control, expt=expt, obs=obs,
     #                   cs_level1=True, eqw_level2=True, level3=False)
 
-    do_sea.sea_compute(varnames, control=None, expt=None, obs=obs,
-                       cs_level1=False, eqw_level2=False,
-                       extreme_level4=True)
+    #do_sea.sea_compute(varnames, control=None, expt=None, obs=obs,
+    #                   cs_level1=False, eqw_level2=False,
+    #                   extreme_level4=True)
